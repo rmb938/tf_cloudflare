8bf191bcb7667a4bd334dd5c0eda276f01025887
Ryan Belgrave <ryan@belgrave.me>
2025-03-16 17:33:12 -0500

    add the test of the openstack controller services

--- hl.rmb938.me.tf
+++ hl.rmb938.me.tf
@@ -166,3 +166,129 @@ resource "cloudflare_record" "openstack-keystone-3" {
   value   = "192.168.23.78"
   type    = "A"
 }
+
+resource "cloudflare_record" "openstack-glance-1" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-glance-1.us-homelab1.hl"
+  value   = "192.168.23.79"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-glance-2" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-glance-2.us-homelab1.hl"
+  value   = "192.168.23.80"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-glance-3" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-glance-3.us-homelab1.hl"
+  value   = "192.168.23.81"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-cinder-1" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-cinder-1.us-homelab1.hl"
+  value   = "192.168.23.82"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-cinder-2" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-cinder-2.us-homelab1.hl"
+  value   = "192.168.23.83"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-cinder-3" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-cinder-3.us-homelab1.hl"
+  value   = "192.168.23.84"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-placement-1" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-placement-1.us-homelab1.hl"
+  value   = "192.168.23.85"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-placement-2" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-placement-2.us-homelab1.hl"
+  value   = "192.168.23.86"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-placement-3" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-placement-3.us-homelab1.hl"
+  value   = "192.168.23.87"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-nova-1" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-nova-1.us-homelab1.hl"
+  value   = "192.168.23.88"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-nova-2" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-nova-2.us-homelab1.hl"
+  value   = "192.168.23.89"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-nova-3" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-nova-3.us-homelab1.hl"
+  value   = "192.168.23.90"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-neutron-1" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-neutron-1.us-homelab1.hl"
+  value   = "192.168.23.91"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-neutron-2" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-neutron-2.us-homelab1.hl"
+  value   = "192.168.23.92"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-neutron-3" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-neutron-3.us-homelab1.hl"
+  value   = "192.168.23.93"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-octavia-1" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-octavia-1.us-homelab1.hl"
+  value   = "192.168.23.94"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-octavia-2" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-neuoctaviatron-2.us-homelab1.hl"
+  value   = "192.168.23.95"
+  type    = "A"
+}
+
+resource "cloudflare_record" "openstack-octavia-3" {
+  zone_id = local.rmb938-me-zone-id
+  name    = "openstack-octavia-3.us-homelab1.hl"
+  value   = "192.168.23.96"
+  type    = "A"
+}
